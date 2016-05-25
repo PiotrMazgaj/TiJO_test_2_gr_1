ca9ea6abe1059d29d30d1bf45904a811a44eee7f
Your Name <you@example.com>
2016-05-25 10:29:05 +0200

    zad2

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -6,19 +6,24 @@ describe('app', function ()
 
     describe('reverseNumber', function () {
         it('should reverse the number', function () {
-            expect(app.reverseNumber(1234)).toEqual(41);
+            expect(app.reverseNumber(1234)).toEqual(4321);
+			expect(app.reverseNumber(2345)).toEqual(5432);
         });
         it('should reverse the number from "number string"', function () {
-            expect(app.reverseNumber('3334433')).toBe('4433344');
+            expect(app.reverseNumber('3334433')).toBe(3344333);
+			expect(app.reverseNumber('1221')).toBe(1221);
         });
         it('should not return a string', function () {
             expect(app.reverseNumber(1234)).not.toBe('4321');
+			expect(app.reverseNumber(1245)).not.toBe('5421');
         });
         it('should not accept not number as an argument', function () {
-            expect(app.reverseNumber([])).toEqual(false);
+			expect(app.reverseNumber([])).not.toEqual(false);
+			expect(app.reverseNumber([1])).not.toEqual(false);
         });
         it('should return single number', function () {
             expect(app.reverseNumber(5)).toBe(5);
+			expect(app.reverseNumber(7)).toBe(7);
         });
     });
 });
